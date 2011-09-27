2a97cd82374d00270f757222550303d346c7a8f2
Reagan Williams <reagan.williams@gmail.com>
2011-09-27 07:23:31 -0700

    hw2-ip

--- hw2/Floor.java
+++ hw2/Floor.java
@@ -5,6 +5,13 @@ public class Floor
 	public static void main(String[] args)
 	{
 		Floor f = new Floor();
+
+        System.out.println(f);
+
+        f.addPassengerBoardRequest();
+        f.addPassengerBoardRequest();
+
+        System.out.println(f);
 	}
 
 	public Floor()
@@ -12,20 +19,56 @@ public class Floor
 		passengersWaitingToBoard = 0;
 	}
 
-	public unloadPassengers(Elevator e)
+	public void unloadPassengers(Elevator e)
 	{
-		
-		e.boardPassengers(1);
+        // call Elevator unload passengers method
+        e.unloadCurrentFloorPassengers();
+
+        System.out.println("Currently " + passengersWaitingToBoard + " passengers waiting to board.");
+
+        // load passengers waiting to board
+        boolean boardResult = true;
+
+        while (boardResult && passengersWaitingToBoard > 0)
+        {
+            try
+            {
+                e.boardPassenger(1);
+                passengersWaitingToBoard--;
+            }
+            catch (CapacityReachedException ex)
+            {
+                System.out.println("Elevator capacity reached: " + ex);
+                boardResult = false;
+            }
+//            boardResult = e.boardPassenger(1);
+//            passengersWaitingToBoard--;
+
+            // according to the reqs this actually needs to be handled/caught by an exception handler.  This is a temp hack at that.
+//            if (!boardResult)
+//            {
+//                System.out.println("Cannot board passenger, elevator at capacity.");
+//                e.registerRequest(e.getCurrentFloor());
+//            }
+
+        }
+
+        System.out.println("Finished looping - now " + passengersWaitingToBoard + " waiting to board");
 	}
 
-	public getPassengerWaitingToBoardCount()
+    public void addPassengerBoardRequest()
+    {
+        passengersWaitingToBoard++;
+    }
+
+	public int getPassengersWaitingToBoardCount()
 	{
 		return passengersWaitingToBoard;
 	}
 
 	public String toString()
     {
-        return "Currently " + getPassengerWaitingToBoardCount() + " passengers waiting to board the elevator.";
+        return "Currently " + passengersWaitingToBoard + " passengers waiting to board the elevator.";
     }
 
 	private int passengersWaitingToBoard;
